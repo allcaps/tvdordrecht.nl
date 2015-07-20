f5c5651c2929ea95bc93c6d14260954d8c8874c6
allcaps <cvanderkamp@gmail.com>
2015-07-21 01:35:54 +0200

    Add settings production

--- tvdordrecht/load_data.sh
+++ tvdordrecht/load_data.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-python manage.py loaddata webapp/fixtures/menu.json
-python manage.py loaddata webapp/fixtures/page.json
-python manage.py loaddata webapp/fixtures/image.json
-python manage.py loaddata webapp/fixtures/news.json
-python manage.py loaddata race/fixtures/data.json
-python manage.py loaddata swimtest/fixtures/data.json
+python manage.py loaddata webapp/fixtures/menu.json --settings=settings.production
+python manage.py loaddata webapp/fixtures/page.json --settings=settings.production
+python manage.py loaddata webapp/fixtures/image.json --settings=settings.prodution
+python manage.py loaddata webapp/fixtures/news.json --settings=settings.production
+python manage.py loaddata race/fixtures/data.json --settings=settings.production
+python manage.py loaddata swimtest/fixtures/data.json --settings=settings.production
