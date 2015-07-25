1fb7a1303562ec9d2e442bcdcf352f90732f8e58
Coen van der Kamp <c.van.der.kamp@zestsoftware.nl>
2015-07-25 04:14:04 +0200

    Add to seconds

--- tvdordrecht/swimtest/templatetags/to_milliseconds.py
+++ tvdordrecht/swimtest/templatetags/to_milliseconds.py
@@ -13,3 +13,16 @@ def to_milliseconds(value):
     seconds += value.minute * 60
     seconds += value.second
     return seconds * 1000
+
+
+@register.filter
+def to_seconds(value):
+    """
+    To seconds
+    For time object to seconds.
+    """
+    seconds = 0
+    seconds += value.hour * 3600
+    seconds += value.minute * 60
+    seconds += value.second
+    return seconds
