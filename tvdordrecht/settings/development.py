bd8845981096a66f98d8afb52d6711965f654ee4
Coen van der Kamp <c.van.der.kamp@zestsoftware.nl>
2015-07-27 00:05:31 +0200

    Add dev settings.

--- tvdordrecht/settings/development.py
+++ tvdordrecht/settings/development.py
@@ -5,9 +5,13 @@ TEMPLATE_DEBUG = DEBUG
 EMAIL_HOST = 'localhost'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+ALLOWED_HOSTS = ['127.0.0.1', ]
+
 INSTALLED_APPS += [
     'debug_toolbar',
 ]
 
 # http://django-debug-toolbar.readthedocs.org/en/latest/installation.html
 INTERNAL_IPS = ('127.0.0.1', )
+
+PREPEND_WWW = False
