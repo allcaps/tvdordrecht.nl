a70d7b93ee2ea4cb8ae99a882cfe0c2f0c838861
Coen van der Kamp <c.van.der.kamp@zestsoftware.nl>
2015-07-29 09:32:42 +0200

    Remove Race and Edition models.

--- tvdordrecht/race/models.py
+++ tvdordrecht/race/models.py
@@ -9,8 +9,6 @@ from webapp.models import Image
 
 """
 An `Event` can have multiple Editions.
-An `Edition` can have multiple Races.
-A `Race` can have multiple Results.
 A `Result` belongs to one User.
 
 A result with time == Null is a who what where. Aka an indication the user will
@@ -54,35 +52,6 @@ class Event(models.Model):
         return reverse('admin:race_event_change', args=(self.id,))
 
 
-class Edition(models.Model):
-    """
-    An Edition can have multiple Race objects.
-    """
-    event = models.ForeignKey(Event)
-    date = models.DateField()
-
-    def __unicode__(self):
-        return defaultfilters.date(self.date, "j F Y")
-
-    class Meta:
-        unique_together = (("event", "date"),)
-        verbose_name = u"editie"
-        verbose_name_plural = u"  Edities"
-        ordering = ["-date", ]
-
-    def get_absolute_url(self):
-        return reverse('race:event_detail', kwargs={'slug': self.event.slug, })
-
-    def get_edit_url(self):
-        return reverse(
-            'race:edition_update',
-            kwargs={
-                'event_slug': self.event.slug,
-                'pk': self.pk,
-            }
-        )
-
-
 class Distance(models.Model):
     """
     Distance is a property of `Race`.
@@ -100,44 +69,17 @@ class Distance(models.Model):
         ordering = ["default", "order", ]
 
 
-class Race(models.Model):
-    """
-    A Race can have multiple Results
-    """
-    edition = models.ForeignKey(Edition)
-    distance = models.ForeignKey(Distance)
-
-    def __unicode__(self):
-        return self.distance.name
-
-    def get_absolute_url(self):
-        return reverse('race:event_detail', args=(self.edition.event.slug,))
-
-    def get_edit_url(self):
-        return reverse(
-            'race:event_update',
-            kargs={
-                'event_slug': self.edition__event.slug,
-                'pk': self.pk,
-            }
-        )
-
-    class Meta:
-        unique_together = (("edition", "distance"),)
-        verbose_name = "wedstrijd"
-        verbose_name_plural = " Wedstrijden"
-        ordering = ['distance']
-
-
 class Result(models.Model):
     """
     Result
 
-    If no time is given, than this is a indication that the user will
+    If no time is given, than that user still has to
     participate aka who-what-where.
     """
     user = models.ForeignKey(User, verbose_name="deelnemer")
-    race = models.ForeignKey(Race, verbose_name="Wedstrijd")
+    event = models.ForeignKey(Event)
+    date = models.DateField()
+    distance = models.ForeignKey(Distance)
     time = models.TimeField("Tijd", blank=True, null=True, help_text="HH:MM:SS")
     remarks = models.CharField(
         "Opmerkingen",
@@ -145,22 +87,17 @@ class Result(models.Model):
         blank=True,
         help_text=u"Podiumplaatsen zijn het vermelden waard. Bijvoorbeeld: '1e H50'.",
         )
+    # Meta fields
+    pub_date = models.DateTimeField("publicatie datum", blank=True, null=True)
+    owner = models.ForeignKey(User, verbose_name="Eigenaar", blank=True, null=True, editable=False, related_name="%(class)s_owner")
+    last_modified_by = models.ForeignKey(User, verbose_name="Laatst bewerkt door", blank=True, null=True, editable=False, related_name="%(class)s_last_modified_by")
+    last_modified = models.DateTimeField("laatst bewerkt", blank=True, null=True, editable=False, auto_now=True)
 
     def __unicode__(self):
-        time = self.time or "(Geen)"
-        return "%s %s" % (self.user, time)
-
-    def event_name(self):
-        return self.race.edition.event
-
-    def edition(self):
-        return self.race.edition
-
-    def choice_label(self):
-        return "%s %s %s" % (self.edition(), self.event_name(), self.race.distance)
+        return "%s %s %s %s %s" % (self.user, self.date, self.event, self.distance, self.time)
 
     class Meta:
-        unique_together = (("user", "race"),)
-        verbose_name = "uitslag"
-        verbose_name_plural = "uitslagen"
-        ordering = ['race__edition', 'time']
+        unique_together = (("user", "event", "date", "distance"),)
+        verbose_name = "wie wat waar / uitslag"
+        verbose_name_plural = "wie wat waars / uitslagen"
+        ordering = ['date', 'event', 'distance', 'time']
