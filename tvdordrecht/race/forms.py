36112e6b44e3b9fe890a2c59ef9460447dccc9e7
Coen van der Kamp <c.van.der.kamp@zestsoftware.nl>
2015-07-31 10:56:25 +0200

    Remove race and edtition forms.

--- tvdordrecht/race/forms.py
+++ tvdordrecht/race/forms.py
@@ -19,40 +19,6 @@ from .models import (
 )
 
 
-# class EditionForm(forms.ModelForm):
-#     distances = forms.ModelMultipleChoiceField(
-#         queryset=Distance.objects.all(),
-#         label='Wedstrijden',
-#         help_text="Selecteer alle wedstrijden die georganiseerd worden.",
-#         required=True,
-#         widget=forms.CheckboxSelectMultiple(),
-#     )
-#
-#     class Meta:
-#         model = Edition
-#         exclude = []
-#
-#     def __init__(self, *args, **kwargs):
-#         super(EditionForm, self).__init__(*args, **kwargs)
-#         self.fields['event'].widget = forms.HiddenInput()
-#         self.fields['date'].label = "Editie (datum)"
-#         self.fields['date'].help_text = "Bijvoorbeeld: 29-08-2015"
-#
-#     def save(self, commit=True):
-#         """
-#         This save method only allows adding new races, but not removing them.
-#         """
-#         edition = super(EditionForm, self).save()
-#         for distance in self.cleaned_data['distances']:
-#             Race.objects.get_or_create(edition=edition, distance=distance)
-
-
-# class RaceForm(forms.ModelForm):
-#     class Meta:
-#         model = Race
-#         exclude = []
-
-
 class EventForm(forms.ModelForm):
     class Meta:
         model = Event
@@ -61,12 +27,6 @@ class EventForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(EventForm, self).__init__(*args, **kwargs)
         self.fields['website'].help_text = None
-
-
-# class ResultForm(forms.ModelForm):
-#     class Meta:
-#         model = Result
-#         exclude = []
 
 
 class WhoWhatWhereEventForm(forms.Form):
@@ -87,23 +47,16 @@ class WhoWhatWhereEventForm(forms.Form):
         )
 
 
-# class WhoWhatWhereEditionForm(forms.Form):
-#     edition = forms.ModelChoiceField(
-#         queryset=Edition.objects.all(),
-#         label='Editie',
-#         required=True,
-#         empty_label=None,
-#         widget=forms.widgets.RadioSelect(),
-#     )
+class WhoWhatWhereDetailForm(forms.ModelForm):
 
+    def __init__(self, *args, **kwargs):
+        super(WhoWhatWhereDetailForm, self).__init__(*args, **kwargs)
+        self.fields['distance'].widget = forms.widgets.RadioSelect()
+        self.fields['distance'].empty_label = None
 
-# class WhoWhatWhereRaceForm(forms.Form):
-#     race = forms.ModelMultipleChoiceField(
-#         queryset=Race.objects.all(),
-#         label='Aan welke wedstrijd ga je deelnemen?',
-#         required=True,
-#         widget=forms.CheckboxSelectMultiple(),
-#     )
+    class Meta:
+        model = Result
+        exclude = ['event', 'pub_date', 'time', 'remarks']
 
 
 class EditionSearchForm(forms.Form):
@@ -134,12 +87,20 @@ class EditionSearchForm(forms.Form):
             )
         )
 
+
+class ResultForm(forms.ModelForm):
+    class Meta:
+        model = Result
+        exclude = []
+
+
 class ResultForm0(forms.Form):
     result = forms.ChoiceField(
         label='Wedstrijd',
         required=True,
         widget=forms.widgets.RadioSelect()
     )
+
 
 class ResultForm1(forms.ModelForm):
     class Meta:
