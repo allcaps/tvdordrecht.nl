d783b48946a3567d5ebcf0234158414f604a86b5
Coen van der Kamp <c.van.der.kamp@zestsoftware.nl>
2016-01-12 23:27:18 +0100

    Close registration.

--- tvdordrecht/tvdordrecht/urls.py
+++ tvdordrecht/tvdordrecht/urls.py
@@ -2,11 +2,16 @@ from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 
 admin.autodiscover()
 
 urlpatterns = [
+    # We closed registration. So here we highjack the create account url
+    # Direct to template won't do anyting with http post.
+    url('^account-aanmaken/$',
+        TemplateView.as_view(template_name='registration/account_form.html'),),
     url('^', include('django.contrib.auth.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^wedstrijd/', include('race.urls', namespace="race")),
